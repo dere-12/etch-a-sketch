f64745ff6de67d28d7e9e0dac7009a1d01ad8c12
Dereje <dereje2012k@gmail.com>
2025-05-03 18:09:34 +0300

    Complete basic features
    implement createGrid function and add a button to create custom grid.

--- script.js
+++ script.js
@@ -1,16 +1,41 @@
 const container = document.querySelector(".container");
+createGrid();
 
-for (let i = 1; i <= 16; i++) {
-  for (let j = 1; j <= 16; j++) {
-    const div = document.createElement("div");
-    container.appendChild(div);
+function createGrid(n = 16) {
+  // n = number of squares per side(like 5x5, 16x16...)
+  const containerWidth = 640;
+  const squareWidthOrHeight = containerWidth / n;
+
+  for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= n; j++) {
+      const div = document.createElement("div");
+      div.style.width = `${squareWidthOrHeight}px`;
+      div.style.height = `${squareWidthOrHeight}px`;
+      container.appendChild(div);
+    }
   }
+
+  const divs = document.querySelectorAll(".container div");
+  divs.forEach((div) => {
+    div.addEventListener("mouseenter", (event) => {
+      const tr = event.target;
+      tr.style.backgroundColor = "black";
+    });
+
+    div.addEventListener("click", (event) => {
+      event.target.style.backgroundColor = "white";
+    });
+  });
 }
 
-const divs = document.querySelectorAll(".container div");
-divs.forEach((div) => {
-  div.addEventListener("mouseenter", (event) => {
-    const tr = event.target;
-    tr.style.backgroundColor = "black";
-  });
+const btn = document.querySelector(".btn");
+btn.addEventListener("click", () => {
+  const n = parseInt(prompt("Enter number of squares per side (1-100)"));
+  if (n < 1) {
+    alert("Zero or Negative numbers are not allowed.");
+  } else if (n > 100) {
+    alert("Please enter a number between 1 and 100.");
+  }
+  container.innerHTML = "";
+  createGrid(n);
 });
